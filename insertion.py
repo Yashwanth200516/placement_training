0f34f1d19a6d074a5aa1be1a61e7e9607d9b7daf
Yashwanth <yashwanthyashu23579@gmail.com>
2026-06-02 22:00:39 +0530

    Added index.html and insertion.py

---
+++ insertion.py
@@ -0,0 +1,14 @@
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+
+        while j>=0 and key<arr[j]:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=key
+
+    return arr
+
+nums=[4,2,1,3]
+print(insertion_sort(nums))
