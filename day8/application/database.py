3dcc7d7118c09473f23380cfc6818855e1dc052a
Yashwanth C <yashwanthyashu23579@gmail.com>
2026-06-11 23:22:20 +0530

    Delete day8/application directory

--- day8/application/database.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-import certifi
-
-client=MongoClient("mongodb+srv://Yashwanth_C:yashu2005@yashwanth-c.ziy5pcf.mongodb.net/",
-    tlsCaFile=certifi.where())
-
-db=client["studentdb"]
-students_collection=db["students"]
